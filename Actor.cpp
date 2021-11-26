a68ef2803b5b37498abaf4106cb04c0ea284e6a0
ChisaiMaikeru <asianavicii@gmail.com>
2021-11-26 14:08:16 -0800

    Skeleton Code for classes

--- Actor.cpp
+++ Actor.cpp
@@ -2,3 +2,65 @@
 #include "StudentWorld.h"
 
 // Students:  Add code to this file (if you wish), Actor.h, StudentWorld.h, and StudentWorld.cpp
+
+/****************************************
+Actor class
+****************************************/
+// Default constructor
+Actor::Actor(int imageID, int startX, int startY, Direction startDirection, float size = 1.0, unsigned int depth = 0) : GraphObject(imageID, startX, startY, startDirection, size, depth) {
+	
+}
+
+// Destructor
+Actor::~Actor() {
+
+}
+
+/****************************************
+Earth class
+****************************************/
+// Default Constructor
+Earth::Earth(int imageID, int startX, int startY, Direction startDirection, float size = 0.25, unsigned int depth = 3) : Actor(imageID, startX, startY, startDirection, size, depth) {
+	setVisible(true);
+}
+
+// This function does nothing, but we have to set it up due to doSomething in Actor being a pure virtual function
+void Earth::doSomething() {
+
+}
+
+// Destructor
+Earth::~Earth() {
+
+}
+
+/****************************************
+Humanoid class
+****************************************/
+// Default constructor
+Humanoid::Humanoid(int imageID, int startX, int startY, Direction startDirection, float size = 1.0, unsigned int depth = 0) : Actor(imageID, startX, startY, startDirection, size, depth) {
+
+}
+
+// Destructor
+Humanoid::~Humanoid() {
+
+}
+
+
+/****************************************
+Tunnelman class
+****************************************/
+// Default constructor
+Tunnelman::Tunnelman(int imageID, int startX, int startY, Direction startDirection, float size = 1.0, unsigned int depth = 0) : Humanoid(imageID, startX, startY, startDirection, size, depth) {
+
+}
+
+void Tunnelman::doSomething() {
+
+}
+
+// Destructor
+Tunnelman::~Tunnelman() {
+
+}
--- Actor.h
+++ Actor.h
@@ -3,6 +3,57 @@
 
 #include "GraphObject.h"
 
-// Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
+// Actor - Abstract base class for all game Objects
+class Actor: public GraphObject {
+public:
+	// Default constructor
+	Actor(int, int, int, Direction, float, unsigned int);
+
+	// This function serves as the default most action that any game object will perform
+	virtual void doSomething() = 0;
+
+	// Destructor
+	virtual ~Actor();
+};
+
+class Earth : public Actor {
+public:
+	// Default constructor
+	Earth(int, int, int, Direction, float, unsigned int);
+
+	// This function does nothing
+	virtual void doSomething();
+
+	// Destructor
+	virtual ~Earth();
+};
+
+// Humanoid - base class used for all human type objects within the game
+class Humanoid : public Actor {
+public:
+	// Default construtor
+	Humanoid(int, int, int, Direction, float, unsigned int);
+
+	// virtual void doSomething() = 0;	// Leaving this line to signify that we are still inheritting this function as a 
+										//	pure virtual function from Actor
+
+	// bool isAnnoyed() = 0;			// TODO: Will not implement this yet, leaving in to show this is not just going to 
+										//	be a copy and paste of Actor.
+
+	// Destructor
+	virtual ~Humanoid();
+};
+
+// Tunnelman - Gameobject that will be used for in game character Tunnelman, notice this class is final
+//	meaning that no other classes may inherit from this class.
+class Tunnelman : public Humanoid {
+public:
+	//Default constructor
+	Tunnelman(int, int, int, Direction, float, unsigned int);
+
+	virtual void doSomething();
+
+	virtual ~Tunnelman();
+};
 
 #endif // ACTOR_H_
