91b25e8c76924b66fa44f6a935380aef85ca932c
andy-phu <82431189+andy-phu@users.noreply.github.com>
2021-11-28 00:36:04 -0800

    tunnelman active

--- StudentWorld.cpp
+++ StudentWorld.cpp
@@ -4,43 +4,48 @@
 #include <memory>
 using namespace std;
 
-StudentWorld::StudentWorld(string assetDir): GameWorld(assetDir){
-}
+StudentWorld::StudentWorld(string assetDir): GameWorld(assetDir){}
 
-StudentWorld::~StudentWorld(){
-    ;
-}
+StudentWorld::~StudentWorld(){}
 
 //skeleton;
 int StudentWorld::init(){
     //tunnelman object getting placed in right spot
-    Tunnelman tMan;
+    Tunnelman* tMan = new Tunnelman();
+    Earth* earthObjects[64][64];
     //Earth earth(int,int);
     //unique_ptr<Actor> earthPtr;
     //each bucket in the 2d array has a unique ptr that points to a vector of unique pointers that point to a game object
-
-    Earth* earthObjects[64][64];
-
+//    vector<unique_ptr<Actor>> V;
+//    unique_ptr<vector<unique_ptr<Actor>>> arrOfVects[64][64];
     //push a pointer to an eart object into the vector in each array bucket
-    //left of mine shaft in mid rows:0-59 cols:0-29
-    for (int r = 0; r < 59; r++){
-        for (int c = 0; c < 30; c++){
-
-            earthObjects[r][c] = new Earth(r, c);
-
+    //left of mine shaft in mid rows:0-29 cols:0-59
+    for (int c = 0; c < 29; c++){
+        for (int r = 0; r< 59; r++){
+            //unique_ptr<Earth> ePtr = make_unique<Earth>Earth(r, c)
+            earthObjects[c][r] = new Earth(c,r);
         }
     }
     //right of mine shaft
-
+    for (int c = 34; c < 64; c++){
+        for (int r = 0; r< 59; r++){
+            //unique_ptr<Earth> ePtr = make_unique<Earth>Earth(r, c)
+            earthObjects[c][r] = new Earth(c,r);
+        }
+    }
+    
+    //earth at the end of mine shaft
+    for (int c = 29; c < 34; c++){
+        for (int r = 0; r< 4; r++){
+            //unique_ptr<Earth> ePtr = make_unique<Earth>Earth(r, c)
+            earthObjects[c][r] = new Earth(c,r);
+        }
+    }
     return GWSTATUS_CONTINUE_GAME;
 }
 
 int StudentWorld::move(){
     return GWSTATUS_CONTINUE_GAME;
-}
-
-void StudentWorld::cleanUp() {
-
 }
 
 GameWorld* createStudentWorld(string assetDir)
--- StudentWorld.h
+++ StudentWorld.h
@@ -16,22 +16,22 @@ public:
     
     ~StudentWorld();
     
-	virtual int init();
-		//{
-		//	return GWSTATUS_CONTINUE_GAME;
-		//}
+    virtual int init();
+	
+//		return GWSTATUS_CONTINUE_GAME;
+	
 
-	virtual int move();
-	//{
-	//	// This code is here merely to allow the game to build, run, and terminate after you hit enter a few times.
-	//	// Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
-	//	decLives();
-	//	return GWSTATUS_PLAYER_DIED;
-	//}
+    virtual int move();
 
-	virtual void cleanUp();
-	//{
-	//}
+		// This code is here merely to allow the game to build, run, and terminate after you hit enter a few times.
+		// Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
+//		decLives();
+//		return GWSTATUS_PLAYER_DIED;
+	
+
+	virtual void cleanUp()
+	{
+	}
 
 private:
 
